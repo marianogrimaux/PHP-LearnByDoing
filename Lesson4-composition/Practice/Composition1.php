a010e77a05e3848626d5961962fe05b441b8146d
marianogrimaux <mariano.grimaux@gmail.com>
2021-01-08 11:27:01 -0300

    WIP

---
+++ Lesson4-composition/Practice/Composition1.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *  Finish the the Car example shown in this lesson.
+ *
+ *  The car has:
+ *   1 Engine.
+ *   4 wheels.
+ *   4 doors.
+ *   1 Console command that has a radio and a GPS
+ *
+ *   When the car rides, a the engine starts working and returns a speed.
+ *   Speed is needed for the wheels to spin.
+ *   If the speed is 0, the wheels will stop spinning.
+ */
---
+++ Lesson4-composition/Practice/Composition2.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ *  From our previous exercise, we now want to add trucks and motorcycles to our model.
+ *  We understand that those are vehicles.
+ *  We know that there are Diesel engines and Gas engines.
+ *  We know that motorcycles have no doors.
+ *  We know that motorcycles have 2 wheels.
+ *  Motorcycles may have a GPS but no radio.
+ *
+ *  Combine Composition with abstract classes in order to solve this problem.
+ *  Code your solution and also create the UML class diagram
+ */
+
